808d3bbc52f8f8efd6f885a1afc0bdab7eaa6585
Denys <hriaznov.dev@gmail.com>
2018-06-24 15:24:16 +0300

    changed readme, added ajax requests for crypto prices

--- src/actions/cryptoInfo.js
+++ src/actions/cryptoInfo.js
@@ -5,7 +5,7 @@ export function getInfo () {
     return dispatch => {
         dispatch(getInfoRequest());
 
-        axios.get(`https://min-api.cryptocompare.com/data/all/coinlist`).then(response => {
+        axios.get('https://min-api.cryptocompare.com/data/all/coinlist').then(response => {
             const cryptoData = response.data.Data;
 
             dispatch(getInfoSuccess(cryptoData));
@@ -23,10 +23,20 @@ export function getInfoRequest () {
 }
 
 export function getInfoSuccess (cryptoData) {
+    let newCryptoData = Object.assign({}, cryptoData);
+
+    for (let key in cryptoData) {
+        axios.get(`https://min-api.cryptocompare.com/data/price?fsym=${key}&tsyms=USD`).then(response => {
+            newCryptoData[key]['Price'] = response.data.USD ? response.data.USD : '';
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+
     return {
         type: types.GET_INFO_SUCCESS,
         loading: false,
-        cryptoData
+        newCryptoData
     }
 }
 
