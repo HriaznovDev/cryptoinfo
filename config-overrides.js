3852ab1022ad781faf72a02c3960903168f2d6c5
Denys <hriaznov.dev@gmail.com>
2018-06-30 19:06:42 +0300

    changed the default ant theme color

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,13 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
 
 /* config-overrides.js */
 module.exports = function override (config, env) {
-  config = rewireReactHotLoader(config, env)
+  config = rewireReactHotLoader(config, env);
+  config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
+  config = rewireLess.withLoaderOptions({
+    modifyVars: { "@primary-color": "#00c69b" },
+  })(config, env);
   return config
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 import { store } from './store/configureStore';
 import { Provider } from 'react-redux';
+import './index.css';
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>,
-    document.getElementById('root'));
+    document.getElementById('root')
+);
 
 registerServiceWorker();
