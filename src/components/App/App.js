08580db5031b3298684944a2e0a051b20ec2524d
Denys <hriaznov.dev@gmail.com>
2018-07-01 10:20:36 +0300

    added react-particles-js for header background

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,14 @@ import { hot } from 'react-hot-loader';
 import { BackTop } from 'antd';
 import './App.css';
 import CryptoContent from '../../containers/CryptoContent/CryptoContent';
+import Particles from 'react-particles-js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <Particles height="180px" />
           <h1 className="App-title">Crypto Info</h1>
         </header>
         <CryptoContent />
